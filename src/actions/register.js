011bdc188c4ab39ebbe6a62ecf3fb1d4fe9c6a64
TheCoTam <hoangnguyenanhtuyen@gmail.com>
2024-07-17 22:37:11 +0700

    hoàn thiện chức năng đăng ký

---
+++ src/actions/register.js
@@ -0,0 +1,29 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "@/lib/firebase";
+import { doc, setDoc } from "firebase/firestore";
+
+export const register = async (values) => {
+  const { email, password, confirmPassword, name } = values;
+  if (password !== confirmPassword) {
+    return { error: "Password does not match" };
+  }
+
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+
+    await setDoc(doc(db, "users", res.user.uid), {
+      id: res.user.uid,
+      name,
+      email,
+      blocked: [],
+    });
+
+    await setDoc(doc(db, "userchats", res.user.uid), {
+      chats: [],
+    });
+    return { success: "Account created" };
+  } catch (error) {
+    console.log("Register", error);
+    return { error: "Something went wrong" };
+  }
+};
